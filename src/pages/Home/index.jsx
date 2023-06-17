108c4c5af515d54e3a604856cae8655c384d0dab
MisterCxmpy <cosgunhussein@gmail.com>
2023-06-17 15:56:19 +0100

    more styling to home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -24,8 +24,32 @@ export default function Home() {
           <button className={styles["race-your-friends-btn"]}>Create Typing test</button>
         </div>
       </div>
-      <div className={styles["profile-panel"]}>
-        
+      <div className={styles["information-panel"]}>
+        <div className={styles["profile-panel"]}>
+          <div className={styles["user"]}>
+            <h1>Welcome Guest!</h1>
+            <h4>Creating an account on Typely will allow you to take advantage of all the features of the site.</h4>
+            <ul>
+              <li>Participate in daily challenges</li>
+              <li>Save your highscores</li>
+              <li>Compete in tournaments</li>
+              <li>Show up in all leaderboards</li>
+              <li>Ability to personalize your profile</li>
+              <li>Exchange coins for in game items and profile customizations</li>
+              <li>Add friends</li>
+            </ul>
+            <button className={styles["create-account-btn"]}>Create Account</button>
+          </div>
+          <div className={styles["divider"]}></div>
+          <div className={styles["friends"]}>
+            <h1>Friends</h1>
+            <h4>Create an account to add friends.</h4>
+          </div>
+        </div>
+        <div className={styles["competitions-panel"]}>
+          <h1>Competitions</h1>
+          <h4>Coming soon!</h4>
+        </div>
       </div>
       <div className={styles["leaderboard-panel"]}>
 
