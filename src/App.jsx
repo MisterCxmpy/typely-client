1d9aab7aefd2e49b7921e5ce043b605730bf8ef3
MisterCxmpy <cosgunhussein@gmail.com>
2023-06-17 15:21:21 +0100

    style home page, add functionality to redirect to practice page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { WordsContainer } from "./components";
 import { Navbar } from "./layouts";
+import { Home } from "./pages";
 
 function App() {
   return (
     <div className="container">
       <Routes>
         <Route path="/" element={<Navbar />}>
-          <Route index element={<WordsContainer />} />
+          <Route index element={<Home />} />
+          <Route path="/practice" element={<WordsContainer />} />
         </Route>
       </Routes>
     </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import styles from "./index.module.css"
+import { useNavigate } from 'react-router-dom'
 
 export default function Home() {
+  const navigate = useNavigate()
+
   return (
-    <h1>Home page</h1>
+    <div className={styles["container"]}>
+      <div className={styles["game-modes"]}>
+        <div className={styles["mode"]}>
+          <h1>Multiplayer</h1>
+          <h3>Increase your typing speed while racing against others</h3>
+          <button className={styles["multiplayer-btn"]}>Enter a Typing race</button>
+        </div>
+        <div className={styles["mode"]}>
+          <h1>Typing Test</h1>
+          <h3>Improve your typing skills on your own</h3>
+          <button onClick={() => navigate("/practice")} className={styles["typing-test-btn"]}>Practice Yourself</button>
+        </div>
+        <div className={styles["mode"]}>
+          <h1>Race your friends</h1>
+          <h3>Create your own typing test and play with friends</h3>
+          <button className={styles["race-your-friends-btn"]}>Create Typing test</button>
+        </div>
+      </div>
+      <div className={styles["profile-panel"]}>
+        
+      </div>
+      <div className={styles["leaderboard-panel"]}>
+
+      </div>
+    </div>
   )
 }
